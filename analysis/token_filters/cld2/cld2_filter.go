08db2eae42517141b8eda63c087c94d83bd95a45
Marty Schoch <marty.schoch@gmail.com>
2014-08-25 11:40:58 -0400

    added alternate build tag 'full' which will be an alias to enable all

--- analysis/token_filters/cld2/cld2_filter.go
+++ analysis/token_filters/cld2/cld2_filter.go
@@ -7,7 +7,7 @@
 //  either express or implied. See the License for the specific language governing permissions
 //  and limitations under the License.
 
-// +build cld2
+// +build cld2 full
 
 package cld2
 
