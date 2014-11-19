eb16b3c56334266c917dd24cbbfc247380cab0c1
Marty Schoch <marty.schoch@gmail.com>
2014-11-19 15:55:09 -0500

    properly return multi-value fields in an array

--- search/search.go
+++ search/search.go
@@ -48,7 +48,20 @@ func (dm *DocumentMatch) AddFieldValue(name string, value interface{}) {
 	if dm.Fields == nil {
 		dm.Fields = make(map[string]interface{})
 	}
-	dm.Fields[name] = value
+	existingVal, ok := dm.Fields[name]
+	if ok {
+		valSlice, ok := existingVal.([]interface{})
+		if ok {
+			// already a slice, append to it
+			valSlice = append(valSlice, value)
+		} else {
+			// create a slice
+			valSlice = []interface{}{existingVal, value}
+		}
+		dm.Fields[name] = valSlice
+	} else {
+		dm.Fields[name] = value
+	}
 }
 
 type DocumentMatchCollection []*DocumentMatch
