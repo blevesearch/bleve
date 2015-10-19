592c1cb2d8b99e44daeb924eb74a2de2f452961c
Ben Campbell <ben@scumways.com>
2015-10-20 06:53:36 +1300

    Remove leftover .tmp files in go gen phase

--- query_string_parser.go
+++ query_string_parser.go
@@ -11,13 +11,15 @@
 //go:generate sed -i.tmp -e s/Lexer/lexer/g query_string.nn.go
 //go:generate sed -i.tmp -e s/Newlexer/newLexer/g query_string.nn.go
 //go:generate sed -i.tmp -e s/debuglexer/debugLexer/g query_string.nn.go
+//go:generate rm query_string.nn.go.tmp
 //go:generate go fmt query_string.nn.go
 //go:generate go tool yacc -o query_string.y.go query_string.y
 //go:generate sed -i.tmp -e 1d query_string.y.go
+//go:generate rm query_string.y.go.tmp
 
 // note: OSX sed and gnu sed handle the -i (in-place) option differently.
-// using -i.tmp works on both, at the expense of leaving a couple of
-// unsightly .tmp files littering the directory...
+// using -i.tmp works on both, at the expense of having to remove
+// the unsightly .tmp files
 
 package bleve
 
