blevesearch/bleve

commit f2aba116c49ea51b27bc9afd3bf15305ef04883c

Make top score collector about 7 times faster

search/collectors/collector_top_score.go
@@ -24,6 +24,7 @@ type TopScoreCollector struct {
 	results       *list.List
 	took          time.Duration
 	maxScore      float64
+	minScore      float64
 	total         uint64
 	facetsBuilder *search.FacetsBuilder
 }
@@ -98,6 +99,10 @@ func (tksc *TopScoreCollector) collectSingle(dm *search.DocumentMatch) {
 		tksc.maxScore = dm.Score
 	}
 
+	if dm.Score <= tksc.minScore {
+		return
+	}
+
 	for e := tksc.results.Front(); e != nil; e = e.Next() {
 		curr := e.Value.(*search.DocumentMatch)
 		if dm.Score < curr.Score {
@@ -106,7 +111,7 @@ func (tksc *TopScoreCollector) collectSingle(dm *search.DocumentMatch) {
 			// if we just made the list too long
 			if tksc.results.Len() > (tksc.k + tksc.skip) {
 				// remove the head
-				tksc.results.Remove(tksc.results.Front())
+				tksc.minScore = tksc.results.Remove(tksc.results.Front()).(*search.DocumentMatch).Score
 			}
 			return
 		}
@@ -115,7 +120,7 @@ func (tksc *TopScoreCollector) collectSingle(dm *search.DocumentMatch) {
 	tksc.results.PushBack(dm)
 	if tksc.results.Len() > (tksc.k + tksc.skip) {
 		// remove the head
-		tksc.results.Remove(tksc.results.Front())
+		tksc.minScore = tksc.results.Remove(tksc.results.Front()).(*search.DocumentMatch).Score
 	}
 }
 

search/collectors/collector_top_score_test.go
@@ -10,8 +10,6 @@
 package collectors
 
 import (
-	"math/rand"
-	"strconv"
 	"testing"
 
 	"golang.org/x/net/context"
@@ -225,27 +223,17 @@ func TestTop10ScoresSkip10(t *testing.T) {
 }
 
 func BenchmarkTop10of100000Scores(b *testing.B) {
+	benchHelper(10000, NewTopScorerCollector(10), b)
+}
 
-	matches := make(search.DocumentMatchCollection, 0, 100000)
-	for i := 0; i < 100000; i++ {
-		matches = append(matches, &search.DocumentMatch{
-			ID:    strconv.Itoa(i),
-			Score: rand.Float64(),
-		})
-	}
-	searcher := &stubSearcher{
-		matches: matches,
-	}
+func BenchmarkTop100of100000Scores(b *testing.B) {
+	benchHelper(10000, NewTopScorerCollector(100), b)
+}
 
-	collector := NewTopScorerCollector(10)
-	b.ResetTimer()
+func BenchmarkTop10of1000000Scores(b *testing.B) {
+	benchHelper(100000, NewTopScorerCollector(10), b)
+}
 
-	err := collector.Collect(context.Background(), searcher)
-	if err != nil {
-		b.Fatal(err)
-	}
-	res := collector.Results()
-	for _, dm := range res {
-		b.Logf("%s - %f\n", dm.ID, dm.Score)
-	}
+func BenchmarkTop100of1000000Scores(b *testing.B) {
+	benchHelper(100000, NewTopScorerCollector(100), b)
 }

search/collectors/bench_test.go
@@ -1,0 +1,32 @@
+package collectors
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+
+	"github.com/blevesearch/bleve/search"
+	"golang.org/x/net/context"
+)
+
+func benchHelper(numOfMatches int, collector search.Collector, b *testing.B) {
+	matches := make(search.DocumentMatchCollection, 0, numOfMatches)
+	for i := 0; i < numOfMatches; i++ {
+		matches = append(matches, &search.DocumentMatch{
+			ID:    strconv.Itoa(i),
+			Score: rand.Float64(),
+		})
+	}
+
+	b.ResetTimer()
+
+	for run := 0; run < b.N; run++ {
+		searcher := &stubSearcher{
+			matches: matches,
+		}
+		err := collector.Collect(context.Background(), searcher)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}

commit 2043bb4bf8775a88ed377ceca0e5a426790eb86c

fix pagination bug introduced by collector optimization
fixes #378

this bug was introduced by:
https://github.com/blevesearch/bleve/commit/f2aba116c49ea51b27bc9afd3bf15305ef04883c

theory of operation for this collector (top N, skip K)

- collect the highest scoring N+K results
- if K > 0, skip K and return the next N

internal details

- the top N+K are kept in a list
- the list is ordered from lowest scoring (first) to highest scoring (last)
- as a hit comes in, we find where this new hit would fit into this list
- if this caused the list to get too big, trim off the head (lowest scoring hit)

theory of the optimization

- we were not tracking the lowest score in the list
- so if the score was lower than the lowest score, we would add/remove it
- by keeping track of the lowest score in the list, we can avoid these ops

problem with the optimization
- the optimization worked by returning early
- by returning early there was a subtle change to documents which had the same score
- the reason is that which docs end up in the top N+K changed by returning early
- why was that? docs are coming in, in order by key ascending
- when finding the correct position to insert a hit into the list, we checked <, not <= the score
- this has the subtle effect that docs with the same score end up in reverse order

for example consider the following in progress list:

doc ids [   c    a    b  ]
scores  [   1    5    9  ]

if we now see doc d with score 5, we get:

doc ids [   c    a    d    b  ]
scores  [   1    5    5    9  ]

While that appears in order (a, d) it is actually reverse order, because when we
produce the top N we start at the end.

theory of the fix

- previous pagination depended on later hits with the same score "bumping" earlier
hits with the same score off the bottom of the list
- however, if we change the logic to <= instead of <, now the list in the previous
example would look like:

doc ids [   c    d    a    b  ]
scores  [   1    5    5    9  ]

search/collectors/collector_top_score.go
@@ -105,7 +105,7 @@ func (tksc *TopScoreCollector) collectSingle(dm *search.DocumentMatch) {
 
 	for e := tksc.results.Front(); e != nil; e = e.Next() {
 		curr := e.Value.(*search.DocumentMatch)
-		if dm.Score < curr.Score {
+		if dm.Score <= curr.Score {
 
 			tksc.results.InsertBefore(dm, e)
 			// if we just made the list too long

search/collectors/collector_top_score_test.go
@@ -222,6 +222,184 @@ func TestTop10ScoresSkip10(t *testing.T) {
 	}
 }
 
+func TestPaginationSameScores(t *testing.T) {
+
+	// a stub search with more than 10 matches
+	// all documents have the same score
+	searcher := &stubSearcher{
+		matches: search.DocumentMatchCollection{
+			&search.DocumentMatch{
+				ID:    "a",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "b",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "c",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "d",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "e",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "f",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "g",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "h",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "i",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "j",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "k",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "l",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "m",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "n",
+				Score: 5,
+			},
+		},
+	}
+
+	// first get first 5 hits
+	collector := NewTopScorerSkipCollector(5, 0)
+	err := collector.Collect(context.Background(), searcher)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	total := collector.Total()
+	if total != 14 {
+		t.Errorf("expected 14 total results, got %d", total)
+	}
+
+	results := collector.Results()
+
+	if len(results) != 5 {
+		t.Fatalf("expected 5 results, got %d", len(results))
+	}
+
+	firstResults := make(map[string]struct{})
+	for _, hit := range results {
+		firstResults[hit.ID] = struct{}{}
+	}
+
+	// a stub search with more than 10 matches
+	// all documents have the same score
+	searcher = &stubSearcher{
+		matches: search.DocumentMatchCollection{
+			&search.DocumentMatch{
+				ID:    "a",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "b",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "c",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "d",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "e",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "f",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "g",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "h",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "i",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "j",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "k",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "l",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "m",
+				Score: 5,
+			},
+			&search.DocumentMatch{
+				ID:    "n",
+				Score: 5,
+			},
+		},
+	}
+
+	// now get next 5 hits
+	collector = NewTopScorerSkipCollector(5, 5)
+	err = collector.Collect(context.Background(), searcher)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	total = collector.Total()
+	if total != 14 {
+		t.Errorf("expected 14 total results, got %d", total)
+	}
+
+	results = collector.Results()
+
+	if len(results) != 5 {
+		t.Fatalf("expected 5 results, got %d", len(results))
+	}
+
+	// make sure that none of these hits repeat ones we saw in the top 5
+	for _, hit := range results {
+		if _, ok := firstResults[hit.ID]; ok {
+			t.Errorf("doc ID %s is in top 5 and next 5 result sets", hit.ID)
+		}
+	}
+
+}
+
 func BenchmarkTop10of100000Scores(b *testing.B) {
 	benchHelper(10000, NewTopScorerCollector(10), b)
 }