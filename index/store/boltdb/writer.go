40c1dc076ffbd820c101f1cf589e325e44fe6d17
slavikm <slavik.markovich@gmail.com>
2016-09-21 16:15:06 +0300

    Now, without the rollback

--- index/store/boltdb/writer.go
+++ index/store/boltdb/writer.go
@@ -51,7 +51,6 @@ func (w *Writer) ExecuteBatch(batch store.KVBatch) (err error) {
 			tx.Rollback()
 		}
 	}()
-	defer tx.Rollback()
 
 	bucket := tx.Bucket([]byte(w.store.bucket))
 	bucket.FillPercent = w.store.fillPercent
