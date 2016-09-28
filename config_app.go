97393d027342f43b17da2f02c090a4e728ac929f
Marty Schoch <marty.schoch@gmail.com>
2016-09-28 18:31:36 -0400

    adding appenginevm build tag

--- config_app.go
+++ config_app.go
@@ -7,7 +7,7 @@
 //  either express or implied. See the License for the specific language governing permissions
 //  and limitations under the License.
 
-// +build appengine
+// +build appengine appenginevm
 
 package bleve
 
