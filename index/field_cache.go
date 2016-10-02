2332455bd20692d0e2e78f921e00e419d98cd08f
Marty Schoch <marty.schoch@gmail.com>
2016-10-02 10:13:14 -0400

    nicer formatting of license header

--- index/field_cache.go
+++ index/field_cache.go
@@ -1,11 +1,16 @@
 //  Copyright (c) 2015 Couchbase, Inc.
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-//  except in compliance with the License. You may obtain a copy of the License at
-//    http://www.apache.org/licenses/LICENSE-2.0
-//  Unless required by applicable law or agreed to in writing, software distributed under the
-//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-//  either express or implied. See the License for the specific language governing permissions
-//  and limitations under the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package index
 
@@ -68,7 +73,7 @@ func (f *FieldCache) FieldNamed(field string, createIfMissing bool) (uint16, boo
 		return index, true
 	}
 	// assign next field id
-	index := f.addLOCKED(field, uint16(f.lastFieldIndex + 1))
+	index := f.addLOCKED(field, uint16(f.lastFieldIndex+1))
 	f.mutex.Unlock()
 	return index, false
 }
