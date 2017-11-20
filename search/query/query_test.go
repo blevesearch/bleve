8b9fd97d59ec7e4e016144fcc39e194a224a7944
Christos Vontas <christos@redsift.io>
2017-11-20 16:53:54 +0000

    add cidr field to parser

--- search/query/query_test.go
+++ search/query/query_test.go
@@ -31,6 +31,7 @@ var startDateStr = "2011-01-01T00:00:00Z"
 var endDateStr = "2012-01-01T00:00:00Z"
 var startDate time.Time
 var endDate time.Time
+var cidr = "192.168.0.1/32"
 
 func init() {
 	var err error
@@ -188,6 +189,10 @@ func TestParseQuery(t *testing.T) {
 			input:  []byte(`{"madeitup":"queryhere"}`),
 			output: nil,
 			err:    true,
+		},
+		{
+			input:  []byte(`{"cidr":"` + cidr + `"}`),
+			output: NewIPRangeQuery(cidr),
 		},
 	}
 
