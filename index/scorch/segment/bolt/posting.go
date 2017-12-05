ece27ef21551f2be790d9fa75a7ce2a798542ea1
Marty Schoch <marty.schoch@gmail.com>
2017-12-05 13:05:12 -0500

    adding initial version of bolt persisted segment

---
+++ index/scorch/segment/bolt/posting.go
@@ -0,0 +1,323 @@
+//  Copyright (c) 2017 Couchbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bolt
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+
+	"github.com/RoaringBitmap/roaring"
+	"github.com/Smerity/govarint"
+	"github.com/blevesearch/bleve/index/scorch/segment"
+	"github.com/boltdb/bolt"
+)
+
+// PostingsList is an in-memory represenation of a postings list
+type PostingsList struct {
+	dictionary *Dictionary
+	term       string
+	postingsID uint64
+	postings   *roaring.Bitmap
+	except     *roaring.Bitmap
+	postingKey []byte
+}
+
+// Iterator returns an iterator for this postings list
+func (p *PostingsList) Iterator() segment.PostingsIterator {
+	rv := &PostingsIterator{
+		postings: p,
+	}
+	if p.postings != nil {
+		detailsBucket := p.dictionary.segment.tx.Bucket(postingDetailsBucket)
+		rv.detailBucket = detailsBucket.Bucket(p.postingKey)
+		rv.all = p.postings.Iterator()
+		if p.except != nil {
+			allExcept := p.postings.Clone()
+			allExcept.AndNot(p.except)
+			rv.actual = allExcept.Iterator()
+		} else {
+			rv.actual = p.postings.Iterator()
+		}
+	}
+
+	return rv
+}
+
+// Count returns the number of items on this postings list
+func (p *PostingsList) Count() uint64 {
+	var rv uint64
+	if p.postings != nil {
+		rv = p.postings.GetCardinality()
+		if p.except != nil {
+			except := p.except.GetCardinality()
+			if except > rv {
+				// avoid underflow
+				except = rv
+			}
+			rv -= except
+		}
+	}
+	return rv
+}
+
+// PostingsIterator provides a way to iterate through the postings list
+type PostingsIterator struct {
+	postings     *PostingsList
+	all          roaring.IntIterable
+	offset       int
+	locoffset    int
+	actual       roaring.IntIterable
+	detailBucket *bolt.Bucket
+
+	currChunk         uint32
+	currChunkFreqNorm []byte
+	currChunkLoc      []byte
+	freqNormDecoder   *govarint.Base128Decoder
+	locDecoder        *govarint.Base128Decoder
+}
+
+func (i *PostingsIterator) loadChunk(chunk int) error {
+	// load correct chunk bytes
+	chunkID := EncodeUvarintAscending(nil, uint64(chunk))
+	chunkBucket := i.detailBucket.Bucket(chunkID)
+	if chunkBucket == nil {
+		return fmt.Errorf("chunk %d missing", chunkID)
+	}
+	i.currChunkFreqNorm = chunkBucket.Get(freqNormKey)
+	i.freqNormDecoder = govarint.NewU64Base128Decoder(bytes.NewReader(i.currChunkFreqNorm))
+	i.currChunkLoc = chunkBucket.Get(locKey)
+	i.locDecoder = govarint.NewU64Base128Decoder(bytes.NewReader(i.currChunkLoc))
+	i.currChunk = uint32(chunk)
+	return nil
+}
+
+func (i *PostingsIterator) readFreqNorm() (uint64, uint64, error) {
+	freq, err := i.freqNormDecoder.GetU64()
+	if err != nil {
+		return 0, 0, fmt.Errorf("error reading frequency: %v", err)
+	}
+	normBits, err := i.freqNormDecoder.GetU64()
+	if err != nil {
+		return 0, 0, fmt.Errorf("error reading norm: %v", err)
+	}
+	return freq, normBits, err
+}
+
+// readLocation processes all the integers on the stream representing a single
+// location.  if you care about it, pass in a non-nil location struct, and we
+// will fill it.  if you don't care about it, pass in nil and we safely consume
+// the contents.
+func (i *PostingsIterator) readLocation(l *Location) error {
+	// read off field
+	fieldID, err := i.locDecoder.GetU64()
+	if err != nil {
+		return fmt.Errorf("error reading location field: %v", err)
+	}
+	// read off pos
+	pos, err := i.locDecoder.GetU64()
+	if err != nil {
+		return fmt.Errorf("error reading location pos: %v", err)
+	}
+	// read off start
+	start, err := i.locDecoder.GetU64()
+	if err != nil {
+		return fmt.Errorf("error reading location start: %v", err)
+	}
+	// read off end
+	end, err := i.locDecoder.GetU64()
+	if err != nil {
+		return fmt.Errorf("error reading location end: %v", err)
+	}
+	// read off num array pos
+	numArrayPos, err := i.locDecoder.GetU64()
+	if err != nil {
+		return fmt.Errorf("error reading location num array pos: %v", err)
+	}
+
+	// group these together for less branching
+	if l != nil {
+		l.field = i.postings.dictionary.segment.fieldsInv[fieldID]
+		l.pos = pos
+		l.start = start
+		l.end = end
+		if numArrayPos > 0 {
+			l.ap = make([]uint64, int(numArrayPos))
+		}
+	}
+
+	// read off array positions
+	for k := 0; k < int(numArrayPos); k++ {
+		ap, err := i.locDecoder.GetU64()
+		if err != nil {
+			return fmt.Errorf("error reading array position: %v", err)
+		}
+		if l != nil {
+			l.ap[k] = ap
+		}
+	}
+
+	return nil
+}
+
+// Next returns the next posting on the postings list, or nil at the end
+func (i *PostingsIterator) Next() (segment.Posting, error) {
+	if i.actual == nil || !i.actual.HasNext() {
+		return nil, nil
+	}
+	n := i.actual.Next()
+	nChunk := n / i.postings.dictionary.segment.chunkFactor
+	allN := i.all.Next()
+	allNChunk := allN / i.postings.dictionary.segment.chunkFactor
+
+	// n is the next actual hit (excluding some postings)
+	// allN is the next hit in the full postings
+	// if they don't match, adjust offsets to factor in item we're skipping over
+	// incr the all iterator, and check again
+	for allN != n {
+
+		// in different chunks, reset offsets
+		if allNChunk != nChunk {
+			i.locoffset = 0
+			i.offset = 0
+		} else {
+
+			if i.currChunk != nChunk || i.currChunkFreqNorm == nil {
+				err := i.loadChunk(int(nChunk))
+				if err != nil {
+					return nil, fmt.Errorf("error loading chunk: %v", err)
+				}
+			}
+
+			// read off freq/offsets even though we don't care about them
+			freq, _, err := i.readFreqNorm()
+			if err != nil {
+				return nil, err
+			}
+			if i.postings.dictionary.segment.fieldsLoc[i.postings.dictionary.fieldID] {
+				for j := 0; j < int(freq); j++ {
+					err := i.readLocation(nil)
+					if err != nil {
+						return nil, err
+					}
+				}
+			}
+
+			// in same chunk, need to account for offsets
+			i.offset++
+		}
+
+		allN = i.all.Next()
+	}
+
+	if i.currChunk != nChunk || i.currChunkFreqNorm == nil {
+		err := i.loadChunk(int(nChunk))
+		if err != nil {
+			return nil, fmt.Errorf("error loading chunk: %v", err)
+		}
+	}
+
+	rv := &Posting{
+		iterator: i,
+		docNum:   uint64(n),
+	}
+
+	var err error
+	var normBits uint64
+	rv.freq, normBits, err = i.readFreqNorm()
+	if err != nil {
+		return nil, err
+	}
+	rv.norm = math.Float32frombits(uint32(normBits))
+	if i.postings.dictionary.segment.fieldsLoc[i.postings.dictionary.fieldID] {
+		// read off 'freq' locations
+		rv.locs = make([]segment.Location, rv.freq)
+		locs := make([]Location, rv.freq)
+		for j := 0; j < int(rv.freq); j++ {
+			err := i.readLocation(&locs[j])
+			if err != nil {
+				return nil, err
+			}
+			rv.locs[j] = &locs[j]
+		}
+	}
+
+	return rv, nil
+}
+
+// Posting is a single entry in a postings list
+type Posting struct {
+	iterator *PostingsIterator
+	docNum   uint64
+
+	freq uint64
+	norm float32
+	locs []segment.Location
+}
+
+// Number returns the document number of this posting in this segment
+func (p *Posting) Number() uint64 {
+	return p.docNum
+}
+
+// Frequency returns the frequence of occurance of this term in this doc/field
+func (p *Posting) Frequency() uint64 {
+	return p.freq
+}
+
+// Norm returns the normalization factor for this posting
+func (p *Posting) Norm() float64 {
+	return float64(p.norm)
+}
+
+// Locations returns the location information for each occurance
+func (p *Posting) Locations() []segment.Location {
+	return p.locs
+}
+
+// Location represents the location of a single occurance
+type Location struct {
+	field string
+	pos   uint64
+	start uint64
+	end   uint64
+	ap    []uint64
+}
+
+// Field returns the name of the field (useful in composite fields to know
+// which original field the value came from)
+func (l *Location) Field() string {
+	return l.field
+}
+
+// Start returns the start byte offset of this occurance
+func (l *Location) Start() uint64 {
+	return l.start
+}
+
+// End returns the end byte offset of this occurance
+func (l *Location) End() uint64 {
+	return l.end
+}
+
+// Pos returns the 1-based phrase position of this occurance
+func (l *Location) Pos() uint64 {
+	return l.pos
+}
+
+// ArrayPositions returns the array position vector associated with this occurance
+func (l *Location) ArrayPositions() []uint64 {
+	return l.ap
+}
---
+++ index/scorch/segment/bolt/segment.go
@@ -0,0 +1,309 @@
+//  Copyright (c) 2017 Couchbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bolt
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	"github.com/RoaringBitmap/roaring"
+	"github.com/Smerity/govarint"
+	"github.com/blevesearch/bleve/index/scorch/segment"
+	"github.com/boltdb/bolt"
+	"github.com/couchbaselabs/vellum"
+	"github.com/golang/snappy"
+)
+
+var readOnlyOptions = &bolt.Options{
+	ReadOnly: true,
+}
+
+// _id field is always guaranteed to have fieldID of 0
+const idFieldID uint16 = 0
+
+// Open returns a boltdb impl of a segment
+func Open(path string) (segment.Segment, error) {
+
+	db, err := bolt.Open(path, 0600, readOnlyOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := db.Begin(false)
+	if err != nil {
+		_ = db.Close()
+		return nil, err
+	}
+
+	rv := &Segment{
+		db:        db,
+		tx:        tx,
+		fieldsMap: make(map[string]uint16),
+	}
+
+	err = rv.loadConfig()
+	if err != nil {
+		_ = db.Close()
+		return nil, err
+	}
+
+	err = rv.loadFields()
+	if err != nil {
+		_ = db.Close()
+		return nil, err
+	}
+
+	return rv, nil
+}
+
+// Segment implements a boltdb based implementation of a segment
+type Segment struct {
+	version     uint8
+	chunkFactor uint32
+	db          *bolt.DB
+	tx          *bolt.Tx
+
+	fieldsMap map[string]uint16
+	fieldsInv []string
+	fieldsLoc []bool
+}
+
+func (s *Segment) loadConfig() (err error) {
+	bucket := s.tx.Bucket(configBucket)
+	if bucket == nil {
+		return fmt.Errorf("config bucket missing")
+	}
+
+	ver := bucket.Get(versionKey)
+	if ver == nil {
+		return fmt.Errorf("version key missing")
+	}
+	s.version = ver[0]
+
+	chunk := bucket.Get(chunkKey)
+	if chunk == nil {
+		return fmt.Errorf("chunk key is missing")
+	}
+	s.chunkFactor = binary.BigEndian.Uint32(chunk)
+
+	return nil
+}
+
+// loadFields reads the fields info from the segment so that we never have to go
+// back to disk to access this (small and used frequently)
+func (s *Segment) loadFields() (err error) {
+
+	bucket := s.tx.Bucket(fieldsBucket)
+	if bucket == nil {
+		return fmt.Errorf("fields bucket missing")
+	}
+
+	indexLocs := roaring.NewBitmap()
+	err = bucket.ForEach(func(k []byte, v []byte) error {
+
+		// process index locations bitset
+		if k[0] == indexLocsKey[0] {
+			_, err2 := indexLocs.FromBuffer(v)
+			if err2 != nil {
+				return fmt.Errorf("error loading indexLocs: %v", err2)
+			}
+		} else {
+
+			_, fieldID, err2 := DecodeUvarintAscending(k)
+			if err2 != nil {
+				return err2
+			}
+			// we store fieldID+1 in so we can discern the zero value
+			s.fieldsMap[string(v)] = uint16(fieldID + 1)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// now setup the inverse (should have same size as map and be keyed 0-(len-1))
+	s.fieldsInv = make([]string, len(s.fieldsMap))
+	for k, v := range s.fieldsMap {
+		s.fieldsInv[int(v)-1] = k
+	}
+	s.fieldsLoc = make([]bool, len(s.fieldsInv))
+	for i := range s.fieldsInv {
+		if indexLocs.ContainsInt(i) {
+			s.fieldsLoc[i] = true
+		}
+	}
+
+	return nil
+}
+
+// Fields returns the field names used in this segment
+func (s *Segment) Fields() []string {
+	return s.fieldsInv
+}
+
+// Count returns the number of documents in this segment
+// (this has no notion of deleted docs)
+func (s *Segment) Count() uint64 {
+	return uint64(s.tx.Bucket(storedBucket).Stats().BucketN - 1)
+}
+
+// Dictionary returns the term dictionary for the specified field
+func (s *Segment) Dictionary(field string) (segment.TermDictionary, error) {
+	return s.dictionary(field)
+}
+
+func (s *Segment) dictionary(field string) (*Dictionary, error) {
+
+	rv := &Dictionary{
+		segment: s,
+		field:   field,
+	}
+
+	rv.fieldID = s.fieldsMap[field]
+	if rv.fieldID > 0 {
+		rv.fieldID = rv.fieldID - 1
+		fieldIDKey := EncodeUvarintAscending(nil, uint64(rv.fieldID))
+		bucket := s.tx.Bucket(dictBucket)
+		if bucket == nil {
+			return nil, fmt.Errorf("dictionary bucket missing")
+		}
+		fstBytes := bucket.Get(fieldIDKey)
+		if fstBytes == nil {
+			return nil, fmt.Errorf("dictionary field %s bytes nil", field)
+		}
+		if fstBytes != nil {
+			fst, err := vellum.Load(fstBytes)
+			if err != nil {
+				return nil, fmt.Errorf("dictionary field %s vellum err: %v", field, err)
+			}
+			if err == nil {
+				rv.fst = fst
+			}
+		}
+
+	}
+
+	return rv, nil
+}
+
+// VisitDocument invokes the DocFieldValueVistor for each stored field
+// for the specified doc number
+func (s *Segment) VisitDocument(num uint64, visitor segment.DocumentFieldValueVisitor) error {
+	storedBuucket := s.tx.Bucket(storedBucket)
+	if storedBuucket == nil {
+		return fmt.Errorf("stored bucket missing")
+	}
+	docNumKey := EncodeUvarintAscending(nil, num)
+	docBucket := storedBuucket.Bucket(docNumKey)
+	if docBucket == nil {
+		return fmt.Errorf("segment has no doc number %d", num)
+	}
+	metaBytes := docBucket.Get(metaKey)
+	if metaBytes == nil {
+		return fmt.Errorf("stored meta bytes for doc number %d is nil", num)
+	}
+	dataBytes := docBucket.Get(dataKey)
+	if dataBytes == nil {
+		return fmt.Errorf("stored data bytes for doc number %d is nil", num)
+	}
+	uncompressed, err := snappy.Decode(nil, dataBytes)
+	if err != nil {
+		return err
+	}
+
+	reader := bytes.NewReader(metaBytes)
+	decoder := govarint.NewU64Base128Decoder(reader)
+
+	keepGoing := true
+	for keepGoing {
+		field, err := decoder.GetU64()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		typ, err := decoder.GetU64()
+		if err != nil {
+			return err
+		}
+		offset, err := decoder.GetU64()
+		if err != nil {
+			return err
+		}
+		l, err := decoder.GetU64()
+		if err != nil {
+			return err
+		}
+		numap, err := decoder.GetU64()
+		if err != nil {
+			return err
+		}
+		var arrayPos []uint64
+		if numap > 0 {
+			arrayPos = make([]uint64, numap)
+			for i := 0; i < int(numap); i++ {
+				ap, err := decoder.GetU64()
+				if err != nil {
+					return err
+				}
+				arrayPos[i] = ap
+			}
+		}
+
+		value := uncompressed[offset : offset+l]
+		keepGoing = visitor(s.fieldsInv[field], byte(typ), value, arrayPos)
+	}
+
+	return nil
+}
+
+// DocNumbers returns a bitset corresponding to the doc numbers of all the
+// provided _id strings
+func (s *Segment) DocNumbers(ids []string) (*roaring.Bitmap, error) {
+	rv := roaring.New()
+
+	if len(s.fieldsMap) > 0 {
+		idDict, err := s.dictionary("_id")
+		if err != nil {
+			return nil, err
+		}
+
+		for _, id := range ids {
+			postings, err := idDict.postingsList(id, nil)
+			if err != nil {
+				return nil, err
+			}
+			if postings.postings != nil {
+				rv.Or(postings.postings)
+			}
+		}
+	}
+
+	return rv, nil
+}
+
+// Close releases all resources associated with this segment
+func (s *Segment) Close() error {
+	err := s.tx.Rollback()
+	if err != nil {
+		_ = s.db.Close()
+		return err
+	}
+	return s.db.Close()
+}
