036b85f047f829754f9b7766743fff05dbd415fb
Michał <kreativka@users.noreply.github.com>
2018-01-22 16:12:52 +0000

    Add config for pl package

--- config/config.go
+++ config/config.go
@@ -94,6 +94,7 @@ import (
 	_ "github.com/blevesearch/bleve/analysis/lang/it"
 	_ "github.com/blevesearch/bleve/analysis/lang/nl"
 	_ "github.com/blevesearch/bleve/analysis/lang/no"
+	_ "github.com/blevesearch/bleve/analysis/lang/pl"
 	_ "github.com/blevesearch/bleve/analysis/lang/pt"
 	_ "github.com/blevesearch/bleve/analysis/lang/ro"
 	_ "github.com/blevesearch/bleve/analysis/lang/ru"
