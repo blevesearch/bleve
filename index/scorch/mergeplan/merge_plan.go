6be678455d7c7c075f3ee857d615cfa99bfca5ea
Sreekanth Sivasankaran <sreekanth.sivasankaran@couchbase.com>
2018-02-02 17:48:50 +0530

    Adjusting the merge task creation loop to accomodate the newly merged segments so that the eventual merge results/ number of segments remain within the calculated budget.

--- index/scorch/mergeplan/merge_plan.go
+++ index/scorch/mergeplan/merge_plan.go
@@ -186,13 +186,14 @@ func plan(segmentsIn []Segment, o *MergePlanOptions) (*MergePlan, error) {
 
 	// While we’re over budget, keep looping, which might produce
 	// another MergeTask.
-	for len(eligibles) > budgetNumSegments {
+	newlyMerged := int(0)
+	for (len(eligibles) + newlyMerged) > budgetNumSegments {
 		// Track a current best roster as we examine and score
 		// potential rosters of merges.
 		var bestRoster []Segment
 		var bestRosterScore float64 // Lower score is better.
 
-		for startIdx := 0; startIdx < len(eligibles)-o.SegmentsPerMergeTask; startIdx++ {
+		for startIdx := 0; startIdx < len(eligibles); startIdx++ {
 			var roster []Segment
 			var rosterLiveSize int64
 
@@ -222,6 +223,7 @@ func plan(segmentsIn []Segment, o *MergePlanOptions) (*MergePlan, error) {
 		rv.Tasks = append(rv.Tasks, &MergeTask{Segments: bestRoster})
 
 		eligibles = removeSegments(eligibles, bestRoster)
+		newlyMerged++
 	}
 
 	return rv, nil
