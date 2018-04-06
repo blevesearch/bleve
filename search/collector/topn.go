4dd4bc7d63c4b191621e930824085df35a2a5b34
Steve Yen <steve.yen@gmail.com>
2018-04-05 23:29:11 -0700

    optimize to return nil facet results when there's no FacetBuilder

--- search/collector/topn.go
+++ search/collector/topn.go
@@ -313,5 +313,5 @@ func (hc *TopNCollector) FacetResults() search.FacetResults {
 	if hc.facetsBuilder != nil {
 		return hc.facetsBuilder.Results()
 	}
-	return search.FacetResults{}
+	return nil
 }
