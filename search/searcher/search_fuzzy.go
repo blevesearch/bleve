ebdbbffd06d86d411f1ba4fbbc10c94370e6ddb4
Marty Schoch <marty.schoch@gmail.com>
2018-05-03 12:54:36 -0400

    limit fuzzy edit distance to 2

--- search/searcher/search_fuzzy.go
+++ search/searcher/search_fuzzy.go
@@ -15,13 +15,22 @@
 package searcher
 
 import (
+	"fmt"
+
 	"github.com/blevesearch/bleve/index"
 	"github.com/blevesearch/bleve/search"
 )
 
+var MaxFuzziness = 2
+
 func NewFuzzySearcher(indexReader index.IndexReader, term string,
 	prefix, fuzziness int, field string, boost float64,
 	options search.SearcherOptions) (search.Searcher, error) {
+
+	if fuzziness > MaxFuzziness {
+		return nil, fmt.Errorf("fuzziness exceeds max (%d)", MaxFuzziness)
+	}
+
 	// Note: we don't byte slice the term for a prefix because of runes.
 	prefixTerm := ""
 	for i, r := range term {
