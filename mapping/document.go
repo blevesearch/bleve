478805bc7f6790f6f088c4d49ca0bee9e443ca10
abhinavdangeti <abhinav@couchbase.com>
2018-09-17 16:46:22 -0700

    Add support for GeoSpatial points in string format
    + Following specification used in elastic search:
        https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-point.html
    + String formats allowed:
        - "lat,lon"
        - "  lat, lon   "
        - "geohash"
    + All the necessary code to decode geohashes is obtained from:
        - https://github.com/mmcloughlin/geohash
    + Also see:
        https://issues.couchbase.com/browse/MB-30542

--- mapping/document.go
+++ mapping/document.go
@@ -424,7 +424,11 @@ func (dm *DocumentMapping) processProperty(property interface{}, path []string, 
 		if subDocMapping != nil {
 			// index by explicit mapping
 			for _, fieldMapping := range subDocMapping.Fields {
-				fieldMapping.processString(propertyValueString, pathString, path, indexes, context)
+				if fieldMapping.Type == "geopoint" {
+					fieldMapping.processGeoPoint(property, pathString, path, indexes, context)
+				} else {
+					fieldMapping.processString(propertyValueString, pathString, path, indexes, context)
+				}
 			}
 		} else if closestDocMapping.Dynamic {
 			// automatic indexing behavior
