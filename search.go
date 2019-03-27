d69eecd015946da003331bb25a74bafef95edb48
Marty Schoch <marty.schoch@gmail.com>
2019-03-27 11:41:46 -0400

    fix incorrectly named method

--- search.go
+++ search.go
@@ -322,9 +322,9 @@ func (r *SearchRequest) SortByCustom(order search.SortOrder) {
 	r.Sort = order
 }
 
-// SetSortAfter sets the request to skip over hits with a sort
+// SetSearchAfter sets the request to skip over hits with a sort
 // value less than the provided sort after key
-func (r SearchRequest) SetSortAfter(after []string) {
+func (r SearchRequest) SetSearchAfter(after []string) {
 	r.SearchAfter = after
 }
 
