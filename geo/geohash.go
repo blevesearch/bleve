2f920ab09a5a94515984d64b6a2821fb1eda2f7c
Sreekanth Sivasankaran <sreekanth.sivasankaran@couchbase.com>
2019-05-10 19:58:20 +0530

    updating copyright header
    adding reference to source

--- geo/geohash.go
+++ geo/geohash.go
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// This implementation is inspired from the geohash-js
+// ref: https://github.com/davetroy/geohash-js
 
 package geo
 
