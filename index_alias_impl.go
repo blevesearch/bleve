5b905c99f4350d29be5a5f9c5ba5becc9aecdcf0
Tyler Kovacs <tyler.kovacs@gmail.com>
2020-05-20 13:11:11 -0700

    Add SortImpl to SearchRequest
    This allows SearchRequests to specify the sort implementation
    (defaults to sort.Sort) to use when sorting hits.

--- index_alias_impl.go
+++ index_alias_impl.go
@@ -16,7 +16,6 @@ package bleve
 
 import (
 	"context"
-	"sort"
 	"sync"
 	"time"
 
@@ -521,10 +520,11 @@ func MultiSearch(ctx context.Context, req *SearchRequest, indexes ...Index) (*Se
 		}
 	}
 
+	sortImpl := req.GetSortImpl()
 	// sort all hits with the requested order
 	if len(req.Sort) > 0 {
 		sorter := newSearchHitSorter(req.Sort, sr.Hits)
-		sort.Sort(sorter)
+		sortImpl(sorter)
 	}
 
 	// now skip over the correct From
@@ -549,7 +549,7 @@ func MultiSearch(ctx context.Context, req *SearchRequest, indexes ...Index) (*Se
 		req.Sort.Reverse()
 		// resort using the original order
 		mhs := newSearchHitSorter(req.Sort, sr.Hits)
-		sort.Sort(mhs)
+		sortImpl(mhs)
 		// reset request
 		req.SearchBefore = req.SearchAfter
 		req.SearchAfter = nil
