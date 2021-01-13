d87bcc6c30fcea52bb4325d100a28e465b65a8e5
Amnon <amnon.cohen@redsift.io>
2021-01-13 16:33:17 +0000

    We need also to test the case that the mask len is not divisible by 8.

--- test/ip_field_test.go
+++ test/ip_field_test.go
@@ -94,6 +94,48 @@ func Test_MultiIpvr4CidrQuery(t *testing.T) {
 	}
 }
 
+func Test_CidrQueryNonDivisibleBy8(t *testing.T) {
+	idx := createIdx(t)
+	defer idx.Close()
+
+	err := idx.Index("id1", doc{"192.168.1.01"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = idx.Index("id2", doc{"192.168.1.02"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = idx.Index("id3", doc{"192.168.2.5"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = idx.Index("id4", doc{"192.168.2.6"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reqStr := `192.168.1.0/30`
+	query := bleve.NewIPRangeQuery(reqStr)
+	query.FieldVal = "ip"
+
+	search := bleve.NewSearchRequest(query)
+	res, err := idx.Search(search)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.Total != 2 {
+		t.Fatalf("failed to find %q, res -> %s", reqStr, res)
+	}
+	if res.Hits[0].ID != "id1" {
+		t.Fatalf("expected %q got %q", "id1", res.Hits[0].ID)
+	}
+	if res.Hits[1].ID != "id2" {
+		t.Fatalf("expected %q got %q", "id2", res.Hits[0].Index)
+	}
+}
+
 func Test_simpleIpv4MatchQuery(t *testing.T) {
 	idx := createIdx(t)
 	defer idx.Close()
