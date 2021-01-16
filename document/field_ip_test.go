3541b5d08f34a534b273fd6bd6d5459bc91b8dc1
Amnon <amnon.cohen@redsift.io>
2021-01-16 19:20:19 +0000

    make TestIPField more explicit in showing how IP's are indexed.

--- document/field_ip_test.go
+++ document/field_ip_test.go
@@ -1,6 +1,7 @@
 package document
 
 import (
+	"bytes"
 	"net"
 	"testing"
 )
@@ -10,6 +11,12 @@ func TestIPField(t *testing.T) {
 	nf.Analyze()
 	if nf.length != 1 {
 		t.Errorf("expected 1 token")
+	}
+	if len(nf.value) != 16 {
+		t.Errorf("stored value should be in 16 byte ipv6 format")
+	}
+	if !bytes.Equal(nf.value, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 192, 168, 1, 1}) {
+		t.Errorf("wrong value stored, expected 192.168.1.1, got %q", nf.value.String())
 	}
 	if len(nf.frequencies) != 1 {
 		t.Errorf("expected 1 token freqs")
