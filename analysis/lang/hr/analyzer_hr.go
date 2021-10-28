7c7c2c49c09c972cd7ff0ad5e7f4685511eee9ac
Abhinav Dangeti <abhinav@couchbase.com>
2021-10-28 11:05:56 -0600

    Fix import paths for hr analyzer and add it to config

--- analysis/lang/hr/analyzer_hr.go
+++ analysis/lang/hr/analyzer_hr.go
@@ -15,11 +15,11 @@
 package hr
 
 import (
-	"github.com/blevesearch/bleve/analysis"
-	"github.com/blevesearch/bleve/registry"
+	"github.com/blevesearch/bleve/v2/analysis"
+	"github.com/blevesearch/bleve/v2/registry"
 
-	"github.com/blevesearch/bleve/analysis/token/lowercase"
-	"github.com/blevesearch/bleve/analysis/tokenizer/unicode"
+	"github.com/blevesearch/bleve/v2/analysis/token/lowercase"
+	"github.com/blevesearch/bleve/v2/analysis/tokenizer/unicode"
 )
 
 // Originated from: http://nlp.ffzg.hr/resources/tools/stemmer-for-croatian/
--- analysis/lang/hr/stop_words_hr.go
+++ analysis/lang/hr/stop_words_hr.go
@@ -15,8 +15,8 @@
 package hr
 
 import (
-	"github.com/blevesearch/bleve/analysis"
-	"github.com/blevesearch/bleve/registry"
+	"github.com/blevesearch/bleve/v2/analysis"
+	"github.com/blevesearch/bleve/v2/registry"
 )
 
 const StopName = "stop_hr"
