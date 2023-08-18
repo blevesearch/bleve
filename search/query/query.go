74a557d9d4826f8137cdc491610902bd6608c413
JiAn-Lim <limjian1990@gmail.com>
2023-08-18 14:27:18 +0900

    Replacing deprecated io/ioutil functions

--- search/query/query.go
+++ search/query/query.go
@@ -18,7 +18,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 
 	"github.com/blevesearch/bleve/v2/mapping"
@@ -26,10 +26,10 @@ import (
 	index "github.com/blevesearch/bleve_index_api"
 )
 
-var logger = log.New(ioutil.Discard, "bleve mapping ", log.LstdFlags)
+var logger = log.New(io.Discard, "bleve mapping ", log.LstdFlags)
 
 // SetLog sets the logger used for logging
-// by default log messages are sent to ioutil.Discard
+// by default log messages are sent to io.Discard
 func SetLog(l *log.Logger) {
 	logger = l
 }
