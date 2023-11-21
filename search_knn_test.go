dc9ec920b6f5f4c005d9e97254e7809db8864eb7
Aditi Ahuja <aditi.ahuja@couchbase.com>
2023-11-21 22:57:47 +0530

    Fixed UT

--- search_knn_test.go
+++ search_knn_test.go
@@ -28,6 +28,14 @@ import (
 	"github.com/blevesearch/bleve/v2/search/query"
 )
 
+func getRandomQueryVec(dims int) []float32 {
+	vec := make([]float32, dims)
+	for i := 0; i < dims; i++ {
+		vec[i] = rand.Float32()
+	}
+	return vec
+}
+
 // Test to see if KNN Operators get added right to the query.
 func TestKNNOperator(t *testing.T) {
 	tmpIndexPath := createTmpIndexPath(t)
@@ -86,10 +94,10 @@ func TestKNNOperator(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	termQuery := query.NewTermQuery("world")
+	termQuery := query.NewTermQuery("2")
 
 	searchRequest := NewSearchRequest(termQuery)
-	queryVec2 := getQueryVec("hilly region worldwide")
+	queryVec2 := getRandomQueryVec(5)
 	searchRequest.AddKNN("vector", queryVec2, 3, 2.0)
 	searchRequest.AddKNN("vector", queryVec2, 2, 1.5)
 	searchRequest.Fields = []string{"content", "vector"}
