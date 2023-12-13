blevesearch/bleve

commit dbdfb6368f65be88a096fb4a258998caf5d3b772

MB-60077: Extended knn validation to include knn operator (#1935)
Co-authored-by: Likith B <likith.b@couchbase.com>

search_knn.go
@@ -195,6 +195,9 @@ func queryWithKNN(req *SearchRequest) (query.Query, error) {
 }
 
 func validateKNN(req *SearchRequest) error {
+	if req.KNNOperator != knnOperatorAnd && req.KNNOperator != knnOperatorOr {
+		return fmt.Errorf("unknown knn operator: %s", req.KNNOperator)
+	}
 	for _, q := range req.KNN {
 		if q.K <= 0 || len(q.Vector) == 0 {
 			return fmt.Errorf("k must be greater than 0 and vector must be non-empty")

commit 1bc9730b3f874caef3a71b92c4bf480c4fa60c60

MB-60077: Accommodate kNNOperator validation better (#1937)
Addresses a bug introduced with
https://github.com/blevesearch/bleve/pull/1935

search_knn.go
@@ -195,7 +195,10 @@ func queryWithKNN(req *SearchRequest) (query.Query, error) {
 }
 
 func validateKNN(req *SearchRequest) error {
-	if req.KNNOperator != knnOperatorAnd && req.KNNOperator != knnOperatorOr {
+	if req.KNN != nil &&
+		req.KNNOperator != "" &&
+		req.KNNOperator != knnOperatorOr &&
+		req.KNNOperator != knnOperatorAnd {
 		return fmt.Errorf("unknown knn operator: %s", req.KNNOperator)
 	}
 	for _, q := range req.KNN {