2c731f17cb0c7adf299ebf1af5d340bda99ddd46
Abhinav Dangeti <abhinav@couchbase.com>
2024-03-26 10:32:57 -0600

    Reset ctrlMsg exactly how the older code does

--- index/scorch/merge.go
+++ index/scorch/merge.go
@@ -99,8 +99,8 @@ OUTER:
 					// continue the workloop on a user triggered cancel
 					if ctrlMsg.doneCh != nil {
 						close(ctrlMsg.doneCh)
-						ctrlMsg = nil
 					}
+					ctrlMsg = nil
 					continue OUTER
 				}
 				if err != nil {
