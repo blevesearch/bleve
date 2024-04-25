2aea878e12818b94c8eae7565649d07857fd08f8
Abhi Dangeti <abhinav@couchbase.com>
2024-04-25 11:44:37 -0600

    Disable upside_down's boltdb tests on darwin arm64 (#2020)
    + Test(s) in this file hang on my computer and it seems they've started
    hanging on the workflow jobs as well.

--- index/upsidedown/store/boltdb/store_test.go
+++ index/upsidedown/store/boltdb/store_test.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !darwin || !arm64
+// +build !darwin !arm64
+
 package boltdb
 
 import (
