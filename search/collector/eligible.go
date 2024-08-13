6602943b332c9b5b915b8a98a46e948a84ba5636
Aditi Ahuja <aditi.ahuja@couchbase.com>
2024-08-13 11:01:49 +0530

    minor collector fixes

--- search/collector/eligible.go
+++ search/collector/eligible.go
@@ -1,3 +1,17 @@
+//  Copyright (c) 2024 Couchbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package collector
 
 import (
@@ -9,22 +23,12 @@ import (
 )
 
 type EligibleCollector struct {
-	size     int
-	total    uint64
-	maxScore float64
-	took     time.Duration
-	results  search.DocumentMatchCollection
+	size    int
+	total   uint64
+	took    time.Duration
+	results search.DocumentMatchCollection
 
 	store collectorStore
-
-	needDocIds   bool
-	neededFields []string
-	cachedDesc   []bool
-
-	lowestMatchOutsideResults *search.DocumentMatch
-	updateFieldVisitor        index.DocValueVisitor
-	dvReader                  index.DocValueReader
-	searchAfter               *search.DocumentMatch
 }
 
 func NewEligibleCollector(size int) *EligibleCollector {
