74041550c0aa98c76bbed7f7ee5f5d94b7abd53f
Aditi Ahuja <aditi.ahuja@couchbase.com>
2024-10-15 18:13:42 +0530

    AddMany instead of Add to a bitmap

--- index/scorch/snapshot_index_vr.go
+++ index/scorch/snapshot_index_vr.go
@@ -64,11 +64,16 @@ type IndexSnapshotVectorReader struct {
 // (AND) operations. Eg. finding the eligible doc IDs present in a segment.
 func (i *IndexSnapshotVectorReader) getEligibleDocIDs() *roaring.Bitmap {
 	res := roaring.NewBitmap()
+	internalDocIDs := make([]uint32, 0, len(i.eligibleDocIDs))
 	// converts the doc IDs to uint32 and returns
 	for _, eligibleDocInternalID := range i.eligibleDocIDs {
-		internalDocID, _ := docInternalToNumber(index.IndexInternalID(eligibleDocInternalID))
-		res.Add(uint32(internalDocID))
+		internalDocID, err := docInternalToNumber(index.IndexInternalID(eligibleDocInternalID))
+		if err != nil {
+			continue
+		}
+		internalDocIDs = append(internalDocIDs, uint32(internalDocID))
 	}
+	res.AddMany(internalDocIDs)
 	return res
 }
 
