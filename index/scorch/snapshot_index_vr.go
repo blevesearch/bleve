a328124a7bd2beb18f1f4f2a2532c7e9b1091ead
Aditi Ahuja <aditi.ahuja@couchbase.com>
2024-10-17 11:14:57 +0530

    added condition for no eligible docs

--- index/scorch/snapshot_index_vr.go
+++ index/scorch/snapshot_index_vr.go
@@ -64,16 +64,18 @@ type IndexSnapshotVectorReader struct {
 // (AND) operations. Eg. finding the eligible doc IDs present in a segment.
 func (i *IndexSnapshotVectorReader) getEligibleDocIDs() *roaring.Bitmap {
 	res := roaring.NewBitmap()
-	internalDocIDs := make([]uint32, 0, len(i.eligibleDocIDs))
-	// converts the doc IDs to uint32 and returns
-	for _, eligibleDocInternalID := range i.eligibleDocIDs {
-		internalDocID, err := docInternalToNumber(index.IndexInternalID(eligibleDocInternalID))
-		if err != nil {
-			continue
+	if len(i.eligibleDocIDs) > 0 {
+		internalDocIDs := make([]uint32, 0, len(i.eligibleDocIDs))
+		// converts the doc IDs to uint32 and returns
+		for _, eligibleDocInternalID := range i.eligibleDocIDs {
+			internalDocID, err := docInternalToNumber(index.IndexInternalID(eligibleDocInternalID))
+			if err != nil {
+				continue
+			}
+			internalDocIDs = append(internalDocIDs, uint32(internalDocID))
 		}
-		internalDocIDs = append(internalDocIDs, uint32(internalDocID))
+		res.AddMany(internalDocIDs)
 	}
-	res.AddMany(internalDocIDs)
 	return res
 }
 
