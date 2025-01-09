f20d781c77515c46835f7a800e4b1613da81f523
Likith B <likith.b@couchbase.com>
2025-01-09 15:32:59 +0530

    MB-57888: New apis for index update

--- index_update.go
+++ index_update.go
@@ -1,4 +1,4 @@
-//  Copyright (c) 2024 Couchbase, Inc.
+//  Copyright (c) 2025 Couchbase, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@ import (
 	index "github.com/blevesearch/bleve_index_api"
 )
 
+// Store all the fields that interact with the data
+// from a document path
 type pathInfo struct {
 	fieldMapInfo []*fieldMapInfo
 	dynamic      bool
@@ -28,15 +30,20 @@ type pathInfo struct {
 	parentPath   string
 }
 
+// Store the field information with respect to the
+// document paths
 type fieldMapInfo struct {
 	fieldMapping *mapping.FieldMapping
 	rootName     string
 	parent       *pathInfo
 }
 
-func deletedFields(ori, upd *mapping.IndexMappingImpl) (map[string]*index.FieldInfo, error) {
-
+// Compare two index mappings to identify all of the updatable changes
+func DeletedFields(ori, upd *mapping.IndexMappingImpl) (map[string]*index.FieldInfo, error) {
 	var err error
+
+	// Check for new mappings present in the type mappings
+	// of the updated compared to the original
 	for name, updDMapping := range upd.TypeMapping {
 		err = checkUpdatedMapping(ori.TypeMapping[name], updDMapping)
 		if err != nil {
@@ -44,6 +51,8 @@ func deletedFields(ori, upd *mapping.IndexMappingImpl) (map[string]*index.FieldI
 		}
 	}
 
+	// Check for new mappings present in the default mappings
+	// of the updated compared to the original
 	err = checkUpdatedMapping(ori.DefaultMapping, upd.DefaultMapping)
 	if err != nil {
 		return nil, err
@@ -52,16 +61,23 @@ func deletedFields(ori, upd *mapping.IndexMappingImpl) (map[string]*index.FieldI
 	oriPaths := make(map[string]*pathInfo)
 	updPaths := make(map[string]*pathInfo)
 
+	// Go through each mapping present in the original
+	// and consolidate according to the document paths
 	for name, oriDMapping := range ori.TypeMapping {
 		addPathInfo(oriPaths, "", oriDMapping, ori, nil, name)
 	}
 	addPathInfo(oriPaths, "", ori.DefaultMapping, ori, nil, "")
 
+	// Go through each mapping present in the updated
+	// and consolidate according to the document paths
 	for name, updDMapping := range upd.TypeMapping {
 		addPathInfo(updPaths, "", updDMapping, ori, nil, name)
 	}
 	addPathInfo(updPaths, "", upd.DefaultMapping, ori, nil, "")
 
+	// Compare both the mappings based on the document paths
+	// and create a list of index, docvalues, store differences
+	// for every single field possible
 	fieldInfo := make(map[string]*index.FieldInfo)
 	for path, info := range oriPaths {
 		err = addFieldInfo(fieldInfo, info, updPaths[path])
@@ -70,6 +86,8 @@ func deletedFields(ori, upd *mapping.IndexMappingImpl) (map[string]*index.FieldI
 		}
 	}
 
+	// Remove entries from the list with no changes between the
+	// original and the updated mapping
 	for name, info := range fieldInfo {
 		if !info.All && !info.Index && !info.DocValues && !info.Store {
 			delete(fieldInfo, name)
@@ -78,10 +96,12 @@ func deletedFields(ori, upd *mapping.IndexMappingImpl) (map[string]*index.FieldI
 	return fieldInfo, nil
 }
 
-// Function to ensure updated document mapping does not contain new field mappings
-// or document mappings
+// Ensures updated document mapping does not contain new
+// field mappings or document mappings
 func checkUpdatedMapping(ori, upd *mapping.DocumentMapping) error {
 
+	// Check to verify both original and updated are not nil
+	// and are enabled before proceeding
 	if ori == nil {
 		if upd == nil || !upd.Enabled {
 			return nil
@@ -94,6 +114,7 @@ func checkUpdatedMapping(ori, upd *mapping.DocumentMapping) error {
 	}
 
 	var err error
+	// Recursively go through the child mappings
 	for name, updDMapping := range upd.Properties {
 		err = checkUpdatedMapping(ori.Properties[name], updDMapping)
 		if err != nil {
@@ -101,6 +122,8 @@ func checkUpdatedMapping(ori, upd *mapping.DocumentMapping) error {
 		}
 	}
 
+	// Simple checks to ensure no new field mappings present
+	// in updated
 	for _, updFMapping := range upd.Fields {
 		var oriFMapping *mapping.FieldMapping
 
@@ -117,13 +140,20 @@ func checkUpdatedMapping(ori, upd *mapping.DocumentMapping) error {
 	return nil
 }
 
+// Adds all of the field mappings while maintaining a tree of the document structure
+// to ensure traversal and verification is possible incase of multiple mappings defined
+// for a single field or multiple document fields' data getting written to a single zapx field
 func addPathInfo(paths map[string]*pathInfo, name string, mp *mapping.DocumentMapping,
 	im *mapping.IndexMappingImpl, parent *pathInfo, rootName string) {
 
+	// Early exit if mapping has been disabled
+	// Comparisions later on will be done with a nil object
 	if !mp.Enabled {
 		return
 	}
 
+	// Consolidate path information like index dynamic across multiple
+	// mappings if path is the same
 	var pInfo *pathInfo
 	if val, ok := paths[name]; ok {
 		pInfo = val
@@ -140,6 +170,7 @@ func addPathInfo(paths map[string]*pathInfo, name string, mp *mapping.DocumentMa
 		pInfo.parentPath = parent.path
 	}
 
+	// Recursively add path information for all child mappings
 	for cName, cMapping := range mp.Properties {
 		var pathName string
 		if name == "" {
@@ -150,6 +181,7 @@ func addPathInfo(paths map[string]*pathInfo, name string, mp *mapping.DocumentMa
 		addPathInfo(paths, pathName, cMapping, im, pInfo, rootName)
 	}
 
+	// Add field mapping information keeping the document structure intact
 	for _, fMap := range mp.Fields {
 		fieldMapInfo := &fieldMapInfo{
 			fieldMapping: fMap,
@@ -162,12 +194,15 @@ func addPathInfo(paths map[string]*pathInfo, name string, mp *mapping.DocumentMa
 	paths[name] = pInfo
 }
 
+// Compare all of the fields at a particular document path and add its field information
 func addFieldInfo(fInfo map[string]*index.FieldInfo, ori, upd *pathInfo) error {
 
 	var info *index.FieldInfo
 	var updated bool
 	var err error
 
+	// Assume deleted or disabled mapping if upd is nil. Checks for ori being nil
+	// or upd having mappings not in orihave already been done before this stage
 	if upd == nil {
 		for _, oriFMapInfo := range ori.fieldMapInfo {
 			info, updated, err = compareFieldMapping(oriFMapInfo.fieldMapping, nil)
@@ -182,6 +217,8 @@ func addFieldInfo(fInfo map[string]*index.FieldInfo, ori, upd *pathInfo) error {
 	} else {
 		for _, oriFMapInfo := range ori.fieldMapInfo {
 			var updFMap *mapping.FieldMapping
+			// For multiple fields at a single document path, compare
+			// only with the matching ones
 			for _, updFMapInfo := range upd.fieldMapInfo {
 				if oriFMapInfo.rootName == updFMapInfo.rootName &&
 					oriFMapInfo.fieldMapping.Name == updFMapInfo.fieldMapping.Name {
@@ -206,31 +243,13 @@ func addFieldInfo(fInfo map[string]*index.FieldInfo, ori, upd *pathInfo) error {
 	return nil
 }
 
-func validateFieldInfo(newInfo *index.FieldInfo, updated bool, fInfo map[string]*index.FieldInfo,
-	ori *pathInfo) error {
-
-	var name string
-	if ori.fieldMapInfo[0].parent.parentPath == "" {
-		name = ori.fieldMapInfo[0].fieldMapping.Name
-	} else {
-		name = ori.fieldMapInfo[0].parent.parentPath + "." + ori.fieldMapInfo[0].fieldMapping.Name
-	}
-	if updated {
-		if ori.dynamic {
-			return fmt.Errorf("updated field is under a dynamic property")
-		}
-	}
-	if oldInfo, ok := fInfo[name]; ok {
-		if oldInfo.All != newInfo.All || oldInfo.Index != newInfo.Index ||
-			oldInfo.DocValues != newInfo.DocValues || oldInfo.Store != newInfo.Store {
-			return fmt.Errorf("updated field impossible to verify because multiple mappings point to the same field name")
-		}
-	} else {
-		fInfo[name] = newInfo
-	}
-	return nil
-}
-
+// Compares two field mappings against each other, checking for changes in index, store, doc values
+// and complete deletiion of the mapping while noting that the changes made are doable based on
+// other values like includeInAll and dynamic
+// first return argument gives an empty fieldInfo if no changes detected
+// second return argument gives a flag indicating whether any changes, if detected, are doable or if
+// update is impossible
+// third argument is an error explaining exactly why the change is not possible
 func compareFieldMapping(original, updated *mapping.FieldMapping) (*index.FieldInfo, bool, error) {
 
 	rv := &index.FieldInfo{}
@@ -312,4 +331,31 @@ func compareFieldMapping(original, updated *mapping.FieldMapping) (*index.FieldI
 		return rv, true, nil
 	}
 	return rv, false, nil
+}
+
+// After identifying changes, validate against the existing changes incase of duplicate fields.
+// In such a situation, any conflicting changes found will abort the update process
+func validateFieldInfo(newInfo *index.FieldInfo, updated bool, fInfo map[string]*index.FieldInfo,
+	ori *pathInfo) error {
+
+	var name string
+	if ori.fieldMapInfo[0].parent.parentPath == "" {
+		name = ori.fieldMapInfo[0].fieldMapping.Name
+	} else {
+		name = ori.fieldMapInfo[0].parent.parentPath + "." + ori.fieldMapInfo[0].fieldMapping.Name
+	}
+	if updated {
+		if ori.dynamic {
+			return fmt.Errorf("updated field is under a dynamic property")
+		}
+	}
+	if oldInfo, ok := fInfo[name]; ok {
+		if oldInfo.All != newInfo.All || oldInfo.Index != newInfo.Index ||
+			oldInfo.DocValues != newInfo.DocValues || oldInfo.Store != newInfo.Store {
+			return fmt.Errorf("updated field impossible to verify because multiple mappings point to the same field name")
+		}
+	} else {
+		fInfo[name] = newInfo
+	}
+	return nil
 }
