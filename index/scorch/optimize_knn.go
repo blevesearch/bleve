402bf8e97a0a5a962e70419309d12241bc5376cb
Likith B <likith.b@couchbase.com>
2025-02-07 15:25:13 +0530

    MB-57888: Naming changes

--- index/scorch/optimize_knn.go
+++ index/scorch/optimize_knn.go
@@ -87,7 +87,7 @@ func (o *OptimizeVR) Finish() error {
 				for field, vrs := range o.vrs {
 					var vecIndex segment_api.VectorIndex
 					var err error
-					if info, ok := o.snapshot.updatedFields[field]; ok && (info.RemoveAll || info.Index) {
+					if info, ok := o.snapshot.updatedFields[field]; ok && (info.Deleted || info.Index) {
 						vecIndex, err = segment.InterpretVectorIndex("",
 							o.requiresFiltering, origSeg.deleted)
 					} else {
