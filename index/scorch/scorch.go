1b2c343e45cec861e18e5959c2c1faf9a159ac66
Thejas-bhat <thejas.orkombu@couchbase.com>
2025-02-27 12:17:18 +0530

    rename stat

--- index/scorch/scorch.go
+++ index/scorch/scorch.go
@@ -658,12 +658,13 @@ func (s *Scorch) StatsMap() map[string]interface{} {
 	m["num_persister_nap_merger_break"] = m["TotPersisterMergerNapBreak"]
 	m["total_compaction_written_bytes"] = m["TotFileMergeWrittenBytes"]
 
-	// the bool stat `index_active` indicates whether the background routines
-	// which causes the index to reach a steady state are still doing some work.
+	// the bool stat `index_bgthreads_active` indicates whether the background routines
+	// (which are responsible for the index to attain a steady state) are still
+	// doing some work.
 	if rootEpoch, ok := m["CurRootEpoch"].(uint64); ok {
 		if lastMergedEpoch, ok := m["LastMergedEpoch"].(uint64); ok {
 			if lastPersistedEpoch, ok := m["LastPersistedEpoch"].(uint64); ok {
-				m["index_active"] = !(lastMergedEpoch == rootEpoch && lastPersistedEpoch == rootEpoch)
+				m["index_bgthreads_active"] = !(lastMergedEpoch == rootEpoch && lastPersistedEpoch == rootEpoch)
 			}
 		}
 	}
