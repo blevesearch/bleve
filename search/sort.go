e39cdcc317a1e77f1075f77b8f906d4ddc1f4371
Rahul Rampure <rahul.rampure@couchbase.com>
2025-12-22 17:24:55 +0530

    fix geoSort

--- search/sort.go
+++ search/sort.go
@@ -683,7 +683,7 @@ type SortGeoDistance struct {
 	Field    string
 	Desc     bool
 	Unit     string
-	values   []string
+	values   [][]byte
 	Lon      float64
 	Lat      float64
 	unitMult float64
@@ -693,19 +693,18 @@ type SortGeoDistance struct {
 // this field has the specified term
 func (s *SortGeoDistance) UpdateVisitor(field string, term []byte) {
 	if field == s.Field {
-		s.values = append(s.values, string(term))
+		s.values = append(s.values, term)
 	}
 }
 
 // Value returns the sort value of the DocumentMatch
-// it also resets the state of this SortField for
+// it also resets the state of this SortGeoDistance for
 // processing the next document
 func (s *SortGeoDistance) Value(i *DocumentMatch) string {
-	iTerms := s.filterTermsByType(s.values)
-	iTerm := s.filterTermsByMode(iTerms)
+	iTerm := s.findPrefixCodedNumericTerm(s.values)
 	s.values = s.values[:0]
 
-	if iTerm == "" {
+	if iTerm == nil {
 		return maxDistance
 	}
 
@@ -739,25 +738,16 @@ func (s *SortGeoDistance) Descending() bool {
 	return s.Desc
 }
 
-func (s *SortGeoDistance) filterTermsByMode(terms []string) string {
-	if len(terms) >= 1 {
-		return terms[0]
-	}
-
-	return ""
-}
-
-// filterTermsByType attempts to make one pass on the terms
-// return only valid prefix coded numbers with shift of 0
-func (s *SortGeoDistance) filterTermsByType(terms []string) []string {
-	var termsWithShiftZero []string
+// findPrefixCodedNumericTerm looks through the provided terms
+// and returns the first valid prefix coded numeric term with shift of 0
+func (s *SortGeoDistance) findPrefixCodedNumericTerm(terms [][]byte) []byte {
 	for _, term := range terms {
-		valid, shift := numeric.ValidPrefixCodedTerm(term)
+		valid, shift := numeric.ValidPrefixCodedTermBytes(term)
 		if valid && shift == 0 {
-			termsWithShiftZero = append(termsWithShiftZero, term)
+			return term
 		}
 	}
-	return termsWithShiftZero
+	return nil
 }
 
 // RequiresDocID says this SearchSort does not require the DocID be loaded
