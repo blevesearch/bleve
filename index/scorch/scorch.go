f552a3d891ac04546593e8329d841ee6a6db8e19
Rahul Rampure <rahul.rampure@couchbase.com>
2026-06-24 11:32:35 +0530

    whitespace fix

--- index/scorch/scorch.go
+++ index/scorch/scorch.go
@@ -51,10 +51,9 @@ type Scorch struct {
 
 	rootLock sync.RWMutex
 
-	root               *IndexSnapshot // holds 1 ref-count on the root
-	rootPersisted      []chan error   // closed when root is persisted
-	persistedCallbacks []index.BatchCallback
-
+	root                 *IndexSnapshot // holds 1 ref-count on the root
+	rootPersisted        []chan error   // closed when root is persisted
+	persistedCallbacks   []index.BatchCallback
 	nextSnapshotEpoch    uint64
 	eligibleForRemoval   []uint64        // Index snapshot epochs that are safe to GC.
 	ineligibleForRemoval map[string]bool // Filenames that should not be GC'ed yet.
